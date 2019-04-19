0dd450d667197f3f0f59c848585e1a8e3ffe6461
Taylor Doyle <tcd56@cornell.edu>
2019-04-19 16:43:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(countdown_with_sleep)
+puts "HAPPY NEW YEAR!"
   while countdown_with_sleep
     puts "#{countdown_with_sleep} SECOND(S)!"
     countdown_with_sleep -= 1
-    puts "HAPPY NEW YEAR!"
   end
 end
